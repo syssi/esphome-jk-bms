dffcee194097a52fcf1f0c832a1bff41f87e5572
Sebastian Muszynski <s.muszynski@constructiva.de>
2021-08-13 19:55:19 +0200

    Add specs

---
+++ components/jk_modbus/__init__.py
@@ -0,0 +1,55 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.cpp_helpers import gpio_pin_expression
+from esphome.components import uart
+from esphome.const import CONF_FLOW_CONTROL_PIN, CONF_ID, CONF_ADDRESS
+from esphome import pins
+
+DEPENDENCIES = ["uart"]
+
+jk_modbus_ns = cg.esphome_ns.namespace("jk_modbus")
+JkModbus = jk_modbus_ns.class_("JkModbus", cg.Component, uart.UARTDevice)
+JkModbusDevice = jk_modbus_ns.class_("JkModbusDevice")
+MULTI_CONF = True
+
+CONF_JK_MODBUS_ID = "jk_modbus_id"
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(JkModbus),
+            cv.Optional(CONF_FLOW_CONTROL_PIN): pins.gpio_output_pin_schema,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+
+async def to_code(config):
+    cg.add_global(jk_modbus_ns.using)
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    await uart.register_uart_device(var, config)
+
+    if CONF_FLOW_CONTROL_PIN in config:
+        pin = await gpio_pin_expression(config[CONF_FLOW_CONTROL_PIN])
+        cg.add(var.set_flow_control_pin(pin))
+
+
+def jk_modbus_device_schema(default_address):
+    schema = {
+        cv.GenerateID(CONF_JK_MODBUS_ID): cv.use_id(JkModbus),
+    }
+    if default_address is None:
+        schema[cv.Required(CONF_ADDRESS)] = cv.hex_uint8_t
+    else:
+        schema[cv.Optional(CONF_ADDRESS, default=default_address)] = cv.hex_uint8_t
+    return cv.Schema(schema)
+
+
+async def register_jk_modbus_device(var, config):
+    parent = await cg.get_variable(config[CONF_JK_MODBUS_ID])
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_address(config[CONF_ADDRESS]))
+    cg.add(parent.register_device(var))
---
+++ components/jk_modbus/jk_modbus.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+
+namespace esphome {
+namespace jk_modbus {
+
+class JkModbusDevice;
+
+class JkModbus : public uart::UARTDevice, public Component {
+ public:
+  JkModbus() = default;
+
+  void setup() override;
+
+  void loop() override;
+
+  void dump_config() override;
+
+  void register_device(JkModbusDevice *device) { this->devices_.push_back(device); }
+
+  float get_setup_priority() const override;
+
+  void send(uint8_t address, uint8_t function, uint16_t start_address, uint16_t register_count);
+
+  void set_flow_control_pin(GPIOPin *flow_control_pin) { this->flow_control_pin_ = flow_control_pin; }
+
+ protected:
+  GPIOPin *flow_control_pin_{nullptr};
+
+  bool parse_jk_modbus_byte_(uint8_t byte);
+
+  std::vector<uint8_t> rx_buffer_;
+  uint32_t last_jk_modbus_byte_{0};
+  std::vector<JkModbusDevice *> devices_;
+};
+
+uint16_t crc16(const uint8_t *data, uint8_t len);
+
+class JkModbusDevice {
+ public:
+  void set_parent(JkModbus *parent) { parent_ = parent; }
+  void set_address(uint8_t address) { address_ = address; }
+  virtual void on_jk_modbus_data(const std::vector<uint8_t> &data) = 0;
+
+  void send(uint8_t function, uint16_t start_address, uint16_t register_count) {
+    this->parent_->send(this->address_, function, start_address, register_count);
+  }
+
+ protected:
+  friend JkModbus;
+
+  JkModbus *parent_;
+  uint8_t address_;
+};
+
+}  // namespace jk_modbus
+}  // namespace esphome
