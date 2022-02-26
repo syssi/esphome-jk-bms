4d948d869b363cbdfd7ad5d3ff92a87bd6c2dc7b
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-02-26 22:07:27 +0100

    Remove unused import

--- components/jk_modbus/__init__.py
+++ components/jk_modbus/__init__.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import uart
 from esphome.const import CONF_ADDRESS, CONF_ID
 
