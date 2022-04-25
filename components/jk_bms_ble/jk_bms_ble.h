fca7f958026120eaa383cff5bc94e1fe9c7dabdf
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-04-25 09:37:38 +0200

    Decode JK04 settings frame

--- components/jk_bms_ble/jk_bms_ble.h
+++ components/jk_bms_ble/jk_bms_ble.h
@@ -171,10 +171,11 @@ class JkBmsBle : public esphome::ble_client::BLEClientNode, public PollingCompon
 
   void assemble_(const uint8_t *data, uint16_t length);
   void decode_(const std::vector<uint8_t> &data);
+  void decode_device_info_(const std::vector<uint8_t> &data);
   void decode_jk02_cell_info_(const std::vector<uint8_t> &data);
   void decode_jk04_cell_info_(const std::vector<uint8_t> &data);
-  void decode_device_info_(const std::vector<uint8_t> &data);
-  void decode_settings_(const std::vector<uint8_t> &data);
+  void decode_jk02_settings_(const std::vector<uint8_t> &data);
+  void decode_jk04_settings_(const std::vector<uint8_t> &data);
   void publish_state_(binary_sensor::BinarySensor *binary_sensor, const bool &state);
   void publish_state_(sensor::Sensor *sensor, float value);
   void publish_state_(switch_::Switch *obj, const bool &state);
