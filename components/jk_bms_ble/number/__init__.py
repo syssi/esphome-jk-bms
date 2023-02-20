8959013b7c1514f06ef37797963a9c4f1be33e69
Sebastian Muszynski <basti@linkt.de>
2023-02-20 13:40:47 +0100

    Fix build issue

--- components/jk_bms_ble/number/__init__.py
+++ components/jk_bms_ble/number/__init__.py
@@ -101,7 +101,7 @@ JkNumber = jk_bms_ble_ns.class_("JkNumber", number.Number, cg.Component)
 JK_NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(JkNumber),
-        cv.Optional(CONF_ICON, default=ICON_EMPTY): number.icon,
+        cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
         cv.Optional(CONF_STEP, default=0.01): cv.float_,
         cv.Optional(CONF_UNIT_OF_MEASUREMENT, default=UNIT_VOLT): cv.string_strict,
         cv.Optional(CONF_MODE, default="BOX"): cv.enum(number.NUMBER_MODES, upper=True),
