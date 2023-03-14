3beb7fcccf629b77ef64f55e6c2254281c412fb1
Sebastian Muszynski <basti@linkt.de>
2023-03-14 11:41:53 +0100

    Make protocol version JK02_32S configurable (#280)
    See #275

--- components/jk_bms_ble/number/jk_number.cpp
+++ components/jk_bms_ble/number/jk_number.cpp
@@ -8,15 +8,21 @@ static const char *const TAG = "jk_bms_ble.number";
 
 void JkNumber::dump_config() { LOG_NUMBER("", "JkBmsBle Number", this); }
 void JkNumber::control(float value) {
-  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02) {
-    uint32_t payload = (uint32_t)(value * this->factor_);
-    if (this->parent_->write_register(this->jk02_holding_register_, payload, sizeof(payload))) {
-      this->publish_state(state);
-    }
+  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK04) {
+    ESP_LOGE(TAG, "This number entity isn't supported by the selected protocol version");
     return;
   }
 
-  ESP_LOGE(TAG, "This number entity isn't supported by the selected protocol version");
+  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_32S) {
+    ESP_LOGW(TAG, "Please be careful! The registers of your BMS model are untested/unverified."
+                  "See https://github.com/syssi/esphome-jk-bms/issues/276");
+  }
+
+  // JK02 & JK02_32S
+  uint32_t payload = (uint32_t)(value * this->factor_);
+  if (this->parent_->write_register(this->jk02_holding_register_, payload, sizeof(payload))) {
+    this->publish_state(state);
+  }
 }
 
 }  // namespace jk_bms_ble
