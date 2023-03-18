f859a8401c99575297e5abb24bd8b3cbf1d554a3
Sebastian Muszynski <basti@linkt.de>
2023-03-18 07:36:56 +0100

    Introduce switch entities of protocol version JK02_32S (#282)

--- components/jk_bms_ble/switch/jk_switch.cpp
+++ components/jk_bms_ble/switch/jk_switch.cpp
@@ -9,23 +9,31 @@ static const char *const TAG = "jk_bms_ble.switch";
 
 void JkSwitch::dump_config() { LOG_SWITCH("", "JkBmsBle Switch", this); }
 void JkSwitch::write_state(bool state) {
-  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK04) {
-    if (this->jk04_holding_register_ == 0x00) {
-      ESP_LOGE(TAG, "This switch isn't supported by the selected protocol version");
-      return;
-    }
-
+  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK04 && this->jk04_holding_register_) {
     if (this->parent_->write_register(this->jk04_holding_register_, (state) ? 0x00000001 : 0x00000000, 0x01)) {
       this->publish_state(state);
     }
     return;
   }
 
-  // JK02 & JK02_32S
-  if (this->parent_->write_register(this->jk02_holding_register_, (state) ? 0x00000001 : 0x00000000, 0x04)) {
-    this->publish_state(state);
+  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02 && this->jk02_holding_register_) {
+    if (this->parent_->write_register(this->jk02_holding_register_, (state) ? 0x00000001 : 0x00000000, 0x04)) {
+      this->publish_state(state);
+    }
+    return;
   }
+
+  if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_32S && this->jk02_32s_holding_register_) {
+    if (this->parent_->write_register(this->jk02_32s_holding_register_, (state) ? 0x00000001 : 0x00000000, 0x04)) {
+      this->publish_state(state);
+    }
+    return;
+  }
+
+  ESP_LOGE(TAG, "This switch isn't supported by the selected protocol version");
 }
+
+bool JkSwitch::assumed_state() { return (this->jk02_32s_holding_register_ == 0x6B); }
 
 }  // namespace jk_bms_ble
 }  // namespace esphome
--- components/jk_bms_ble/switch/jk_switch.h
+++ components/jk_bms_ble/switch/jk_switch.h
@@ -11,21 +11,26 @@ class JkBmsBle;
 class JkSwitch : public switch_::Switch, public Component {
  public:
   void set_parent(JkBmsBle *parent) { this->parent_ = parent; };
+  void set_jk04_holding_register(uint8_t jk04_holding_register) {
+    this->jk04_holding_register_ = jk04_holding_register;
+  };
   void set_jk02_holding_register(uint8_t jk02_holding_register) {
     this->jk02_holding_register_ = jk02_holding_register;
   };
-  void set_jk04_holding_register(uint8_t jk04_holding_register) {
-    this->jk04_holding_register_ = jk04_holding_register;
+  void set_jk02_32s_holding_register(uint8_t jk02_32s_holding_register) {
+    this->jk02_32s_holding_register_ = jk02_32s_holding_register;
   };
   void dump_config() override;
   void loop() override {}
   float get_setup_priority() const override { return setup_priority::DATA; }
 
  protected:
+  bool assumed_state() override;
   void write_state(bool state) override;
   JkBmsBle *parent_;
-  uint8_t jk02_holding_register_;
   uint8_t jk04_holding_register_;
+  uint8_t jk02_holding_register_;
+  uint8_t jk02_32s_holding_register_;
 };
 
 }  // namespace jk_bms_ble
