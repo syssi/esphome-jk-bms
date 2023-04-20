8e47137724215434f88518654a3cde6d7bbf1a1d
Sebastian Muszynski <basti@linkt.de>
2023-04-20 10:42:31 +0200

    Add ESPHome 2023.4.0 compatibility

--- components/jk_bms_ble/number/jk_number.cpp
+++ components/jk_bms_ble/number/jk_number.cpp
@@ -9,7 +9,7 @@ static const char *const TAG = "jk_bms_ble.number";
 void JkNumber::dump_config() { LOG_NUMBER("", "JkBmsBle Number", this); }
 void JkNumber::control(float value) {
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK04 && this->jk04_holding_register_) {
-    uint32_t payload = (uint32_t)(value * this->factor_);
+    uint32_t payload = (uint32_t) (value * this->factor_);
     if (this->parent_->write_register(this->jk04_holding_register_, payload, sizeof(payload))) {
       this->publish_state(state);
     }
@@ -17,7 +17,7 @@ void JkNumber::control(float value) {
   }
 
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02 && this->jk02_holding_register_) {
-    uint32_t payload = (uint32_t)(value * this->factor_);
+    uint32_t payload = (uint32_t) (value * this->factor_);
     if (this->parent_->write_register(this->jk02_holding_register_, payload, sizeof(payload))) {
       this->publish_state(state);
     }
@@ -25,7 +25,7 @@ void JkNumber::control(float value) {
   }
 
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_32S && this->jk02_32s_holding_register_) {
-    uint32_t payload = (uint32_t)(value * this->factor_);
+    uint32_t payload = (uint32_t) (value * this->factor_);
     if (this->parent_->write_register(this->jk02_32s_holding_register_, payload, sizeof(payload))) {
       this->publish_state(state);
     }
