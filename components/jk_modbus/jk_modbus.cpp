e079935ca3a8963087e3c2526d13c6e67224e247
Sebastian Muszynski <basti@linkt.de>
2023-06-12 18:22:03 +0200

    Fix format

--- components/jk_modbus/jk_modbus.cpp
+++ components/jk_modbus/jk_modbus.cpp
@@ -11,7 +11,7 @@ void JkModbus::loop() {
   const uint32_t now = millis();
   if (now - this->last_jk_modbus_byte_ > this->rx_timeout_) {
     ESP_LOGVV(TAG, "Buffer cleared due to timeout: %s",
-             format_hex_pretty(&this->rx_buffer_.front(), this->rx_buffer_.size()).c_str());
+              format_hex_pretty(&this->rx_buffer_.front(), this->rx_buffer_.size()).c_str());
     this->rx_buffer_.clear();
     this->last_jk_modbus_byte_ = now;
   }
@@ -23,7 +23,7 @@ void JkModbus::loop() {
       this->last_jk_modbus_byte_ = now;
     } else {
       ESP_LOGVV(TAG, "Buffer cleared due to reset: %s",
-               format_hex_pretty(&this->rx_buffer_.front(), this->rx_buffer_.size()).c_str());
+                format_hex_pretty(&this->rx_buffer_.front(), this->rx_buffer_.size()).c_str());
       this->rx_buffer_.clear();
     }
   }
