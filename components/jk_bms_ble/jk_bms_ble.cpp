3435d6eb054f1d6e72ef6145d1ae18cfee76bf67
Darter <rfdarter@googlemail.com>
2024-01-26 00:18:39 +0100

    removed protocol auto detection

--- components/jk_bms_ble/jk_bms_ble.cpp
+++ components/jk_bms_ble/jk_bms_ble.cpp
@@ -317,16 +317,25 @@ void JkBmsBle::decode_jk02_cell_info_(const std::vector<uint8_t> &data) {
 
   uint8_t offset = 0;
   uint8_t frame_version = FRAME_VERSION_JK02;
-  if (this->protocol_version_ == PROTOCOL_VERSION_JK02) {
-    // Weak assumption: The value of data[189] (JK02) or data[189+32] (JK02_32S) is 0x01, 0x02 or 0x03
-    if (data[189] == 0x00 && data[189 + 32] > 0) {
-      frame_version = FRAME_VERSION_JK02_32S;
-      offset = 16;
-      ESP_LOGW(TAG,
-               "You hit the unstable auto detection of the protocol version. This feature will be removed in future!"
-               "Please update your configuration to protocol version JK02_32S if you are using a JK-B2A8S20P v11+");
-    }
-  }
+  // if (this->protocol_version_ == PROTOCOL_VERSION_JK02) {
+  //   Weak assumption: The value of data[189] (JK02) or data[189+32] (JK02_32S) is 0x01, 0x02 or 0x03
+  //   if (data[189] == 0x00 && data[189 + 32] > 0) {
+  //     frame_version = FRAME_VERSION_JK02_32S;
+  //     offset = 16;
+  //     ESP_LOGW(TAG,
+  //              "You hit the unstable auto detection of the protocol version. This feature will be removed in future!"
+  //              "Please update your configuration to protocol version JK02_32S if you are using a JK-B2A8S20P v11+");
+  //   }
+  //   /**
+  //    * definitly not correct
+  //    * JK B2A20S20P
+  //    * Hardware version: V11 X.W
+  //    * This is the old stile bms but still supports heating
+  //    * max 24 cells
+  //    * offset should be 0
+  //    * 
+  //   */
+  // }
 
   // Override unstable auto detection
   if (this->protocol_version_ == PROTOCOL_VERSION_JK02_32S) {
