fd8908ce9b562025538c3519f2c0e873bfc537b0
Darter <rfdarter@googlemail.com>
2024-02-14 02:07:32 +0100

    fix Number components state getting published with the wrong value #450

--- components/jk_bms_ble/number/jk_number.cpp
+++ components/jk_bms_ble/number/jk_number.cpp
@@ -11,7 +11,7 @@ void JkNumber::control(float value) {
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK04 && this->jk04_holding_register_) {
     uint32_t payload = (uint32_t) (value * this->factor_);
     if (this->parent_->write_register(this->jk04_holding_register_, payload, sizeof(payload))) {
-      this->publish_state(state);
+      this->publish_state(value);
     }
     return;
   }
@@ -19,7 +19,7 @@ void JkNumber::control(float value) {
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_24S && this->jk02_holding_register_) {
     uint32_t payload = (uint32_t) (value * this->factor_);
     if (this->parent_->write_register(this->jk02_holding_register_, payload, sizeof(payload))) {
-      this->publish_state(state);
+      this->publish_state(value);
     }
     return;
   }
@@ -27,7 +27,7 @@ void JkNumber::control(float value) {
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_32S && this->jk02_32s_holding_register_) {
     uint32_t payload = (uint32_t) (value * this->factor_);
     if (this->parent_->write_register(this->jk02_32s_holding_register_, payload, sizeof(payload))) {
-      this->publish_state(state);
+      this->publish_state(value);
     }
     return;
   }
