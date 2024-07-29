63d7dbd5710061e32512c6e32a5bc4a404c45160
Sebastian Muszynski <basti@linkt.de>
2024-07-29 09:24:50 +0200

    Make "RCV Time" and "RFV Time" controllable

--- components/jk_bms_ble/number/jk_number.cpp
+++ components/jk_bms_ble/number/jk_number.cpp
@@ -10,7 +10,7 @@ void JkNumber::dump_config() { LOG_NUMBER("", "JkBmsBle Number", this); }
 void JkNumber::control(float value) {
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK04 && this->jk04_holding_register_) {
     uint32_t payload = (uint32_t) (value * this->factor_);
-    if (this->parent_->write_register(this->jk04_holding_register_, payload, sizeof(payload))) {
+    if (this->parent_->write_register(this->jk04_holding_register_, payload, this->length_)) {
       this->publish_state(value);
     }
     return;
@@ -18,7 +18,7 @@ void JkNumber::control(float value) {
 
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_24S && this->jk02_holding_register_) {
     uint32_t payload = (uint32_t) (value * this->factor_);
-    if (this->parent_->write_register(this->jk02_holding_register_, payload, sizeof(payload))) {
+    if (this->parent_->write_register(this->jk02_holding_register_, payload, this->length_)) {
       this->publish_state(value);
     }
     return;
@@ -26,7 +26,7 @@ void JkNumber::control(float value) {
 
   if (this->parent_->get_protocol_version() == PROTOCOL_VERSION_JK02_32S && this->jk02_32s_holding_register_) {
     uint32_t payload = (uint32_t) (value * this->factor_);
-    if (this->parent_->write_register(this->jk02_32s_holding_register_, payload, sizeof(payload))) {
+    if (this->parent_->write_register(this->jk02_32s_holding_register_, payload, this->length_)) {
       this->publish_state(value);
     }
     return;
