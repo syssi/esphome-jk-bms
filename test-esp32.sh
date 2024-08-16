0be53453ca6a60d5369acf0c5d6d85dd8b83da74
Sebastian Muszynski <basti@linkt.de>
2024-08-16 08:18:53 +0200

    Simplify testing

--- test-esp32.sh
+++ test-esp32.sh
@@ -6,4 +6,4 @@ else
   C="components"
 fi
 
-esphome -s external_components_source $C ${1:-run} ${2:-esp32-example-faker.yaml}
+esphome -s mac_address 20:22:09:27:38:60 -s external_components_source $C ${1:-run} ${2:-esp32-example-faker.yaml}
