5e6492249252e06813ce64e1bcad00cafa499017
Sebastian Muszynski <basti@linkt.de>
2025-07-13 17:56:16 +0200

    Fix lint issues

--- components/jk_modbus/__init__.py
+++ components/jk_modbus/__init__.py
@@ -1,7 +1,7 @@
+from esphome import pins
 import esphome.codegen as cg
 from esphome.components import uart
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.const import CONF_ADDRESS, CONF_FLOW_CONTROL_PIN, CONF_ID
 from esphome.cpp_helpers import gpio_pin_expression
 
