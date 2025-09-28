1726a9ca6f89c91c804db388dbbf96c3dbed09f4
Sebastian Muszynski <basti@linkt.de>
2025-09-28 08:37:32 +0200

    Fix lint issue

--- components/heltec_balancer_ble/heltec_balancer_ble.cpp
+++ components/heltec_balancer_ble/heltec_balancer_ble.cpp
@@ -818,7 +818,7 @@ bool HeltecBalancerBle::send_command(uint8_t function, uint8_t command, uint8_t 
                                sizeof(frame), frame, ESP_GATT_WRITE_TYPE_NO_RSP, ESP_GATT_AUTH_REQ_NONE);
 
   if (status) {
-      ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", this->parent_->address_str().c_str(), status);
+    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", this->parent_->address_str().c_str(), status);
   } 
 
   return (status == 0);
