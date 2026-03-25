6efdd2b42ab190cdf3a04e967b860de3cfa43219
Sebastian Muszynski <basti@linkt.de>
2026-03-25 10:48:55 +0100

    Apply pre-commit fixes

--- components/jk_balancer/text_sensor.py
+++ components/jk_balancer/text_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 
 from . import CONF_JK_BALANCER_ID, JK_BALANCER_COMPONENT_SCHEMA
 
