8ce76b1a1658ce1ec4cfb569098715f9674ddbc3
Sebastian Muszynski <basti@linkt.de>
2026-03-27 14:48:25 +0100

    Use STATE_CLASS_TOTAL_INCREASING for counter sensors (#918)

--- components/jk_bms/sensor.py
+++ components/jk_bms/sensor.py
@@ -533,14 +533,14 @@ CONFIG_SCHEMA = JK_BMS_COMPONENT_SCHEMA.extend(
             icon=ICON_CHARGING_CYCLES,
             accuracy_decimals=0,
             device_class=DEVICE_CLASS_EMPTY,
-            state_class=STATE_CLASS_MEASUREMENT,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional(CONF_TOTAL_CHARGING_CYCLE_CAPACITY): sensor.sensor_schema(
             unit_of_measurement=UNIT_AMPERE_HOURS,
             icon=ICON_COUNTER,
             accuracy_decimals=0,
             device_class=DEVICE_CLASS_EMPTY,
-            state_class=STATE_CLASS_MEASUREMENT,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional(CONF_BATTERY_STRINGS): sensor.sensor_schema(
             unit_of_measurement=UNIT_EMPTY,
